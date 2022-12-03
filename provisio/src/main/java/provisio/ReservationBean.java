77cfcb4c1c7d9e102f71057c32ebe8f90e33776a
Jaira McNair <91927147+jairamcnair@users.noreply.github.com>
2022-12-02 19:05:22 -0500

    Update ReservationBean.java
    fixed loyalty points calculation

--- provisio/src/main/java/provisio/ReservationBean.java
+++ provisio/src/main/java/provisio/ReservationBean.java
@@ -1,4 +1,5 @@
 package provisio;
+import java.time.YearMonth;
 
 public class ReservationBean extends createServlet {
 	
@@ -116,7 +117,19 @@ public class ReservationBean extends createServlet {
 		this.loyalty_points = loyalty_points;
 	}
 	
+	public static int getNumberOfDaysInMonth(int year,int month)
+    {
+        YearMonth yearMonthObject = YearMonth.of(year, month);
+        int daysInMonth = yearMonthObject.lengthOfMonth();
+        return daysInMonth;
+    }
+	
+	
+	// pass in the month to find out how many days are in month
 	public int calculatePoints(String check_in, String check_out) {
+		
+		int points = 0;
+		
 		char checkin_day1 = check_in.charAt(8);  // find the date's first number
 		char checkin_day2 = check_in.charAt(9);  // find the date's second number 
 		String checkin_day = Character.toString(checkin_day1) + Character.toString(checkin_day2);  // put date together
@@ -125,18 +138,47 @@ public class ReservationBean extends createServlet {
 		char checkout_day2 = check_out.charAt(9);  // find the date's second num
 		String checkout_day = Character.toString(checkout_day1) + Character.toString(checkout_day2);  // put date together
 
-		int points = (Integer.parseInt(checkout_day) - Integer.parseInt(checkin_day))*150;  // calculate loyalty points
+		char checkin_month1 = check_in.charAt(5);
+		char checkin_month2 = check_in.charAt(6);
+		String checkin_month = Character.toString(checkin_month1) + Character.toString(checkin_month2);
+		int checkin_monthint = Integer.parseInt(checkin_month); // convert checkin month to integer to pass to numofdaysinmonth function
+		
+		char checkin_year1 = check_in.charAt(0);
+		char checkin_year2 = check_in.charAt(1);
+		char checkin_year3 = check_in.charAt(2);
+		char checkin_year4 = check_in.charAt(3);
+		String checkin_year = Character.toString(checkin_year1) + Character.toString(checkin_year2)+ Character.toString(checkin_year3)+ Character.toString(checkin_year4);  // put date together
+		int checkin_yearint = Integer.parseInt(checkin_year); // convert checkin month to integer to pass to numofdaysinmonth function
+		
+	
+		
+		if(Integer.parseInt(checkout_day) > Integer.parseInt(checkin_day)) {
+			points = (Integer.parseInt(checkout_day) - Integer.parseInt(checkin_day))*150;  // calculate loyalty points
+			return points;
+		}
+		
+		
+		if(Integer.parseInt(checkout_day) < Integer.parseInt(checkin_day)) {
+			int numberOfDaysInCheckinMonth = getNumberOfDaysInMonth(checkin_yearint, checkin_monthint);
+	
+			
+			int part1 = (numberOfDaysInCheckinMonth) - (Integer.parseInt(checkin_day)) + 1;
+			int part2 = (Integer.parseInt(checkout_day) - 1);
+			points = (part1 + part2) * 150;
+			return points;
+		
+		}
 		
 		return points;
 		
+		
 	}
-	
 	public String arrayToString(String[] amenities) {
 		
 		String amenities_string = " ";
 		for(int i = 0; i < amenities.length;++i) {
 			String addition = amenities[i].toString();
-			amenities_string += addition + " ";
+			amenities_string += addition;
 		}
 		return amenities_string;
 	}
@@ -161,4 +203,11 @@ public class ReservationBean extends createServlet {
 		return hotel_ID;
 	}
 	
+
+	
 }
+
+/*
+ * helpful source for getting days in month
+ * https://java2blog.com/get-number-of-days-month-java/#:~:text=You%20can%20use%20LocalDate%20's,days%20in%20Month%20in%20java.
+ */
